239b3dd00a3f352d843fa2a12e8478b54f031201
Dung Nguyen Dang <dangdungcntt@gmail.com>
2018-10-14 15:35:41 +0700

    Add api get movie details

--- src/api.js
+++ src/api.js
@@ -6,6 +6,14 @@ async function responseAsDOM(response) {
     return parser.parseFromString(text, "text/html");
 }
 
+async function readBodyAndDecode(response) {
+  const buffer = await response.arrayBuffer();
+
+  const decoder = new TextDecoder("iso-8859-1");
+  return decoder.decode(buffer);
+}
+
+
 async function getPage(path) {
 	let response = await fetch(`${CORS}/https://www.rottentomatoes.com/${path}`);
 	return responseAsDOM(response);
@@ -22,5 +30,14 @@ export default {
 	    }
 	  }
 	  throw new Error("Failed to fetch movies");
+	},
+
+	async fetchMovie(id) {
+  		const response = await fetch(
+		    `https://www.rottentomatoes.com/api/private/v1.0/movies/${id}.json`
+		);
+		const text = await readBodyAndDecode(response);
+
+    	return JSON.parse(text);
 	}
 }
